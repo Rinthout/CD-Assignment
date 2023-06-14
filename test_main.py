38ff4639c26a91b3f9e440cfcf814e87cb7f2ef4
Rinthout <95086904+Rinthout@users.noreply.github.com>
2023-06-14 21:50:34 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 import main 
 
 def test_index():
-    assert main.index() == 'Hello, world!'
+    assert main.index() == 'Hello, CD assignment!'
     
 def test_cow():
     assert main.cow() == 'MOoooOo!'
