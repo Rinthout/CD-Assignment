8805d18f28e77dd6489c9259d5c60fe66cb81826
Rinthout <95086904+Rinthout@users.noreply.github.com>
2023-06-21 10:41:42 +0200

    Update cd_run.sh

--- cd_run.sh
+++ cd_run.sh
@@ -1,10 +1,9 @@
 # Goto $GITHUB_WORKSPACE
 cd $GITHUB_WORKSPACE
+
 # Do something extremely useful for continous deployment
 git pull origin main
-# Hmmm, we should probably restart the application after we've pulled in
-# new code, otherwise we can be looking for what went wrong for quite a
-# while..
 systemctl restart CD-Assignment
+
 # Verify the application is running
 systemctl status CD-Assignment
