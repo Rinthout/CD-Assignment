fb21a46355197edfc1937ef3bc78854cee6153c5
Rinthout <95086904+Rinthout@users.noreply.github.com>
2023-06-21 21:18:36 +0200

    Update cd_run.sh

--- cd_run.sh
+++ cd_run.sh
@@ -1,9 +1,3 @@
-# Goto $GITHUB_WORKSPACE
 cd $GITHUB_WORKSPACE
-
-# Do something extremely useful for continous deployment
 git pull origin main
-systemctl restart CD-Assignment
-
-# Verify the application is running
-systemctl status CD-Assignment
+systemctl restart nginx
