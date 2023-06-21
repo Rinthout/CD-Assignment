11ae79a0a5b515f3f3ac0a2c72bec638d86db7f7
Rinthout <95086904+Rinthout@users.noreply.github.com>
2023-06-21 10:41:07 +0200

    Update cd_run.sh

--- cd_run.sh
+++ cd_run.sh
@@ -1,14 +1,10 @@
-echo "Here is some code."
-echo "We wrote it in Bash."
-echo "Bash is just a programming language, really."
-# This is a comment.
-# Let's read a file.
-cat some_file.txt
+# Goto $GITHUB_WORKSPACE
+cd $GITHUB_WORKSPACE
 # Do something extremely useful for continous deployment
-git pull
+git pull origin main
 # Hmmm, we should probably restart the application after we've pulled in
 # new code, otherwise we can be looking for what went wrong for quite a
 # while..
-systemctl restart my-application
+systemctl restart CD-Assignment
 # Verify the application is running
-systemctl status my-application
+systemctl status CD-Assignment
