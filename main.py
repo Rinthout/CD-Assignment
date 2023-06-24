cd1d43905720d6cfa47411885adc35102f57df7c
Rinthout <95086904+Rinthout@users.noreply.github.com>
2023-06-24 16:36:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
-    return 'Hello, CD assignment!!!@'
+    return 'Hello, CD assignment!!!@$'
 
 @app.route('/cow')
 def cow():
